59f0a7057872aff375b26e028193aec240169128
anitamas423 <anitamasibo@gmail.com>
2025-08-24 15:24:46 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,19 +10,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  plugins: [react(), tailwindcss(),
+  plugins: [react()],
   base: './'
-  ],
-  server: {
-    host: true,     // allow LAN/0.0.0.0 access
-    port: 5173,     // fixed port
-    strictPort: true, // if 5173 is taken, don't auto-switch
-    allowedHosts: [
-      'localhost',
-      '127.0.0.1',
-      '.ngrok-free.app',   // allow ngrok tunnel
-      '.loca.lt',          // allow localtunnel
-      '.trycloudflare.com' // allow cloudflare tunnel
-    ]
-  }
 })
