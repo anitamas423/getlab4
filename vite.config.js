f4a611ad8584030c24a7accaddea8c2f3991d85b
anitamas423 <anitamasibo@gmail.com>
2025-08-26 05:48:50 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,15 +10,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  plugins: [react()],
-  base: './',
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name].js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name].[ext]'
-      }
-    }
-  }
-});
+  plugins: [react()]
+
+})
